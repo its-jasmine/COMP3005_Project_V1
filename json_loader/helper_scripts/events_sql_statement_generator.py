a568f7b72365c692a3aeafecc4321a0e885ccc13
nive024 <nive0204@gmail.com>
2024-04-03 22:06:41 -0400

    generate general event statements

--- json_loader/helper_scripts/events_sql_statement_generator.py
+++ json_loader/helper_scripts/events_sql_statement_generator.py
@@ -1,4 +1,27 @@
 import json
+import os
+
+columns_names = keys = [
+    "event_id",
+    "match_id",
+    "index",
+    "period",
+    "timestamp",
+    "minute",
+    "second",
+    "type",
+    "possession",
+    "possession_team_id",
+    "play_pattern",
+    "team_id",
+    "duration",
+    "location",
+    "under_pressure",
+    "off_camera",
+    "out",
+    "related_events"
+]
+
 
 def generate_create_statement():
     return "CREATE TABLE IF NOT EXISTS matches \n" \
@@ -10,8 +33,8 @@ def generate_create_statement():
            "minute               INTEGER NOT NULL, \n" \
            "second               INTEGER NOT NULL, \n" \
            "type                 VARCHAR(255) NOT NULL, \n" \
-           "posession            INTEGER NOT NULL, \n" \
-           "posession_team_id    INTEGER NOT NULL, \n" \
+           "possession            INTEGER NOT NULL, \n" \
+           "possession_team_id    INTEGER NOT NULL, \n" \
            "play_pattern         VARCHAR(255) NOT NULL, \n" \
            "team_id              INTEGER, \n" \
            "duration             FLOAT, \n" \
@@ -23,3 +46,80 @@ def generate_create_statement():
 
 # match_id = name of file
 # I'm confused
+def generate_insert_statement(table_name, data, match_id):
+    statements = []
+    values = []
+    values.append(data["id"])
+    values.append(match_id)
+    values.append(data["index"])
+    values.append(data["period"])
+    values.append(data["timestamp"])
+    values.append(data["minute"])
+    values.append(data["second"])
+    values.append(data["type"]["name"])
+    values.append(data["possession"])
+    values.append(data["possession_team"]["id"]) 
+    values.append(data["play_pattern"]["name"])
+    values.append(data["team"]["id"]) 
+    try:
+        values.append(data["duration"])
+    except KeyError:
+        pass
+
+    try:
+        values.append(data["location"])
+    except KeyError:
+        pass
+
+    try:
+        values.append(data["under_pressure"])
+    except KeyError:
+        pass
+
+    try:
+        values.append(data["off_camera"])
+    except KeyError:
+        pass
+
+    try:
+        values.append(data["out"])
+    except KeyError:
+        pass
+
+    try:
+        values.append(data["related_events"])
+    except KeyError:
+        pass
+
+    columns = ', '.join(columns_names) 
+    values = ', '.join(map(repr, values))
+    statements.append(f"INSERT INTO {table_name} ({columns}) VALUES ({values})"  + " ON CONFLICT (event_id) DO NOTHING;") # TODO add IF NOT EXISTS
+    return statements
+
+
+def convert_json_to_sql(file_path):
+    with open(f"../statsbomb_data/events/{file_path}", 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+    
+    statements = []
+    for data in json_data:
+        statements += (generate_insert_statement("events", data, os.path.splitext(file_path)[0])) # Replace 'YourTableName' with your actual table name
+    
+    return statements
+
+
+sql_statements = []
+sql_statements.append(generate_create_statement())
+directory = "../statsbomb_data/events/"
+files = os.listdir(directory)
+
+# for file in files:
+#     sql_statements += convert_json_to_sql(file)
+
+sql_statements += convert_json_to_sql(files[0]) #just to test 
+
+sql_statements = set(sql_statements) # deduplicate
+for statement in sql_statements:
+    statement = statement.replace("None", "NULL")
+    # execute_query(statement)
+    print(statement)
