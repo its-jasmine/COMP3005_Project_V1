19e62306cd395f8757b0112a3859acf76b062026
nive024 <nive0204@gmail.com>
2024-04-06 12:45:06 -0400

    liedwe

--- json_loader/helper_scripts/lineup_sql_statement_generator.py
+++ json_loader/helper_scripts/lineup_sql_statement_generator.py
@@ -4,22 +4,16 @@ columns_names = ['goalkeeper', 'right_back', 'right_center_back', 'left_center_b
 
 def generate_create_statement():
     return "CREATE TABLE IF NOT EXISTS lineups \n" \
-            "(goalkeeper            VARCHAR(255) NOT NULL PRIMARY KEY, \n" \
-            "right_back             INTEGER NOT NULL,  \n" \
-            "right_center_back                INTEGER NOT NULL, \n" \
-            "left_center_back               INTEGER NOT NULL, \n" \
-            "left_back            TIME NOT NULL, \n" \
-            "minute               INTEGER NOT NULL, \n" \
-            "second               INTEGER NOT NULL, \n" \
-            "type                 VARCHAR(255) NOT NULL, \n" \
-            "possession            INTEGER NOT NULL, \n" \
-            "possession_team_id    INTEGER NOT NULL, \n" \
-            "play_pattern         VARCHAR(255) NOT NULL, \n" \
-            "team_id              INTEGER, \n" \
-            "duration             FLOAT, \n" \
-            "location             INTEGER, \n" \
-            "under_pressure       INTEGER, \n" \
-            "off_camera           INTEGER, \n" \
-            "out                  INTEGER, \n" \
-            "related_events       INTEGER);"
+            "(lineup_id                 INTEGER NOT NULL PRIMARY KEY, \n" \
+            "goalkeeper                 INTEGER NOT NULL,  \n" \
+            "right_back                 INTEGER NOT NULL,  \n" \
+            "right_center_back          INTEGER NOT NULL, \n" \
+            "left_center_back           INTEGER NOT NULL, \n" \
+            "left_back                  INTEGER NOT NULL, \n" \
+            "right_defensive_midfield   INTEGER NOT NULL, \n" \
+            "left_defensive_midfield    INTEGER NOT NULL, \n" \
+            "right_midfield             INTEGER NOT NULL, \n" \
+            "left_midfield              INTEGER NOT NULL, \n" \
+            "right_center_forward       INTEGER NOT NULL, \n" \
+            "left_center_forward        INTEGER NOT NULL);"
 
